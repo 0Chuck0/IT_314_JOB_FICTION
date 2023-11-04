faba5eebd4ada82ac4912ecabe4e81226d5eae39
Saumya Shah <sshah89003@gmail.com>
2023-11-04 13:21:17 +0530

    Added Experience Slider

--- Frontend/Main/jobs/script.js
+++ Frontend/Main/jobs/script.js
@@ -1,10 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+
+    /*=============== RANGE SLIDER JS ===============*/
+    const rangeThumb = document.getElementById('range-thumb'),
+        rangeNumber = document.getElementById('range-number'),
+        rangeLine = document.getElementById('range-line'),
+        rangeInput = document.getElementById('range-input')
+
+    const rangeInputSlider = () => {
+        // Insert the value of the input range
+        rangeNumber.textContent = rangeInput.value
+
+        // Calculate the position of the input thumb
+        // rangeInput.value = 50, rangeInput.max = 100, (50 / 100 = 0.5)
+        // rangeInput.offsetWidth = 248, rangeThumb.offsetWidth = 32, (248 - 32 = 216)
+        const thumbPosition = (rangeInput.value / rangeInput.max),
+            space = rangeInput.offsetWidth - rangeThumb.offsetWidth
+
+        // We insert the position of the input thumb
+        // thumbPosition = 0.5, space = 216 (0.5 * 216 = 108)
+        rangeThumb.style.left = (thumbPosition * space) + 'px'
+
+        // We insert the width to the slider with the value of the input value
+        // rangeInput.value = 50, ancho = 50%
+        rangeLine.style.width = rangeInput.value + '%'
+
+        // We call the range input
+        rangeInput.addEventListener('input', rangeInputSlider)
+        rangeNumber.textContent = rangeInput.value/20
+    }
+    rangeInputSlider();
     const Joblistcontainer = document.getElementById('jobListing-container');
     const jobListingTemplate = Handlebars.compile(document.getElementById('job-template').innerHTML);
 
     // Define the func function outside of the event listener
-    async function func() {
+    async function display_jobs() {
         try {
             const formdata = new URLSearchParams(new FormData(document.getElementById("filter-form")));
             const response = await fetch("http://localhost:5000/jobs_main", {
@@ -18,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             }).then(datas => {
                 console.log(datas);
-                Joblistcontainer.innerHTML ="";
+                Joblistcontainer.innerHTML = "";
                 // Loop through the job data and populate the HTML container
                 datas.forEach((job) => {
                     // Create a new job listing using the Handlebars template

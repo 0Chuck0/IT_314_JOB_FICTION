139513776192e67a42b069e39520cac016b02a8f
Saumya Shah <sshah89003@gmail.com>
2023-11-16 00:57:20 +0530

    Added Applied and Recommendations Page
    -->Created 2 new pages : Applied Jobs and Recommendations
    --> Changed side-nav to make it more responsive

--- Frontend/Saved Jobs/side-nav.js
+++ Frontend/Saved Jobs/side-nav.js
@@ -1,7 +1,6 @@
 
 function openNav() {
-  document.getElementById("side-nav-bar").style.width = "20rem";
-  document.getElementById("main").style.marginRight = "20rem";
+  document.getElementById("side-nav-bar").style.width = "22rem";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   document.getElementById("nav-bar-btn").style.width = "0";
   setTimeout(() => {
@@ -11,7 +10,6 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("side-nav-bar").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
   document.body.style.backgroundColor = "white";
   document.getElementById("nav-bar-btn").style.width = "3.5rem";
   document.getElementById("nav-bar-btn").style.display = "block";

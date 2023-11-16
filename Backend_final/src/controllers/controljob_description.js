14acfe5f3ac04220bdb6dbff95e30bb6c86613b6
Shekhar gupta <theshekhargupta125@gmail.com>
2023-11-16 17:23:53 +0530

    added jobs post list and post job functionality

--- Backend_final/src/controllers/controljob_description.js
+++ Backend_final/src/controllers/controljob_description.js
@@ -1,65 +1,42 @@
 const express = require("express");
 const app = express()
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
+const cookieParser=require("cookie-parser");
 app.use(cookieParser());
 const Savedpost = require('../models/savePostSchema');
 const Register = require('../models/registers');
-const jobs = require('../models/jobs');
 
 module.exports = {
-
-  get: async (req, res) => {
-
-    if (req.cookies.jwt) {
-      jwt.verify(req.cookies.jwt, 'ehewlkjjfsafasjflkasfjjkfsjflkasjffjsjasfasffafa', async (err, decoded) => {
-        if (err) {
-          res.status(400).send('<script>alert("Cookies decoding Error."); window.location = "/login";</script>');
+    
+    get:async (req,res)=>{
+        
+        if(req.cookies.jwt){
+            jwt.verify(req.cookies.jwt,'ehewlkjjfsafasjflkasfjjkfsjflkasjffjsjasfasffafa',async(err,decoded)=>{
+            if(err)
+            {
+              res.status(400).send('<script>alert("Cookies decoding Error."); window.location = "/login";</script>');
+            }
+            else
+            {
+                const check = await Register.findOne({_id:decoded._id});
+                req.body.email = check.email;
+                
+                const data = await Register.findOne({email:req.body.email});
+             
+                if(await Savedpost.findOne({job_id:req.params.id, email:data.email}).count() == 1)
+                {
+                   res.render("job_description",{jobid : req.params.id,how: "fa-solid"})
+                }
+                else
+                {
+                  res.render("job_description",{jobid : req.params.id,how: "fa-regular"})
+                }
+            }
+            });
         }
-        else {
-          const check = await Register.findOne({ _id: decoded._id });
-          req.body.email = check.email;
-
-          const data = await Register.findOne({ email: req.body.email });
-
-          if (await Savedpost.findOne({ job_id: req.params.id, email: data.email }).count() == 1) {
-            if (0 === req.params.id.lenght) {
-              res.send('error');
-            }
-            const job = await jobs.findOne({ id: parseInt(req.params.id) });
-
-            if (job) {
-              res.render("job_description", { job, how: "fa-regular" })
-            } else {
-              res.send('error');
-            }
-          }
-          else {
-            if (0 === req.params.id.lenght) {
-              res.send('error');
-            }
-            const job = await jobs.findOne({ id: parseInt(req.params.id) });
-
-            if (job) {
-              res.render("job_description", { job, how: "fa-regular" })
-            } else {
-              res.send('error');
-            }
-          }
+        else
+        {
+           res.render("job_description",{jobid : req.params.id,how: "fa-regular"})
         }
-      });
     }
-    else {
-      if (0 === req.params.id.lenght) {
-        res.send('error');
-      }
-      const job = await jobs.findOne({ id: parseInt(req.params.id) });
-
-      if (job) {
-        res.render("job_description", { job, how: "fa-regular" })
-      } else {
-        res.send('error');
-      }
-    }
-  }
 }
--- Backend_final/src/models/jobs.js
+++ Backend_final/src/models/jobs.js
@@ -1,52 +1,72 @@
 const mongoose=require("mongoose");
 // const Schema = mongoose.Schema;
-const UserScema=new mongoose.Schema({
-    id: {
-        type:Number,
-        unique:true,
-        required:true
-    },
+const Postschema=new mongoose.Schema({
+    id: { 
+        type: Number,
+        unique: true, 
+        required: true,
+    }, 
     job_title: {
-        type:String,
-        
-        required:true
-    },
-    company : {
-        type:String,
-        required:true
-    },
-    salary: {
-        type:Number,
-        required:true
-    },
-    location: {
         type: String,
-        required:true,
-       
+        required: true,
     },
-
-    work_mode:{
+    role:{
         type: String,
-        required:true,
+        require: true,
     },
-
-    education:{
-        type: String,
-        required:true,
-    },
-
     experience:{
         type: Number,
         required:true
     },
-
     skills: [{
         type: String
 
-      }]
+    }],
+    industry_type: {
+        type:String,
+       
+    },
+    employment_type: {
+        type:String
+
+    },
+    perk: [{
+        type:String
+
+    }],
+    salary: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    last_date: {
+        type: Date,
+        require:true,
+    },
+    degree: {
+        type: String
+       
+    },
+   
+    work_mode: {
+        type: String,
+    },
+    criteria: {
+        type: String,
+    },
+
+    company_email:{
+        type: String
+    }
+
+
 }) 
 
 
+
 // now we need to create Collection
-const jobs= mongoose.model("jobs",UserScema) ;
+const jobs= mongoose.model("jobs",Postschema) ;
 module.exports=jobs;

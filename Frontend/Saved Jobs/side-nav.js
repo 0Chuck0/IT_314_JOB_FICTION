a1fe4701d7806ffa448ae38dfe3eabd75771f8a7
Saumya Shah <sshah89003@gmail.com>
2023-11-17 23:33:41 +0530

    Adjusted Profile Icon
    --> Adjusted Profile Icon properly
    --> Added the profile section in toggle menu and hide the profile icon

--- Frontend/Saved Jobs/side-nav.js
+++ Frontend/Saved Jobs/side-nav.js
@@ -1,18 +1,14 @@
 
 function openNav() {
-  document.getElementById("side-nav-bar").style.width = "22rem";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("side-nav-bar").style.width = "22em";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.)";
   document.getElementById("nav-bar-btn").style.width = "0";
-  setTimeout(() => {
-    document.getElementById("nav-bar-btn").style.display = "none";
-  }, 200);
 }
 
 function closeNav() {
   document.getElementById("side-nav-bar").style.width = "0";
   document.body.style.backgroundColor = "white";
   document.getElementById("nav-bar-btn").style.width = "3.5rem";
-  document.getElementById("nav-bar-btn").style.display = "block";
 }
 
 const cardsContainer = document.getElementById('cards-container');

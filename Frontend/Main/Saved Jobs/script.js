943d8a157b97eddbcc4c08333e362a0f1241358f
Karan Jivanramjiwala <karanhj786@gmail.com>
2023-11-18 01:33:47 +0530

    Changes

---
+++ Frontend/Main/Saved Jobs/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const unsaved = document.getElementsByClassName("saved");
+    for (let x of unsaved) {
+        x.addEventListener("click", () => {
+            let classname = x.className;
+            if (classname == "far fa-bookmark saved")
+                x.className = "fas fa-bookmark saved";
+            else
+                x.className = "far fa-bookmark saved";
+        })
+    }
+})

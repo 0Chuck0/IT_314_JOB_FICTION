2fa9a49217b434beb8c221e795dcb56b11459eff
Dabhi_Dipak_202101229 <202101229@gmail.com>
2023-11-22 16:25:10 +0530

    some changes in backend.

--- Backend_final/src/middlewares/upload.js
+++ Backend_final/src/middlewares/upload.js
@@ -1,18 +1,17 @@
 const multer = require("multer");
-const {GridFsStorage} = require("multer-gridfs-storage");
+const { GridFsStorage } = require("multer-gridfs-storage");
 
 const storage = new GridFsStorage({
     url: "mongodb://127.0.0.1:27017/Randome",
-    options: { useNewUrlParser: true, useUnifiedTopology: true }, 
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
 
-        const match = ["image/png", "image/jpeg" , "image/jpg"];
+        const match = ["image/png", "image/jpeg", "image/jpg"];
 
         if (match.indexOf(file.mimetype) === -1) {
-            const filename = `${Date.now()}-image-${file.originalname}`;
-            return filename;
-        }else{
-            console.log("file not upload becuase it doesnt match to formate");
+            const error = new Error("File format not supported");
+            console.log("File format not supported");
+            return ('<script> alert("file format not matched"); window.location ="./" </script> ');
         }
 
         return {

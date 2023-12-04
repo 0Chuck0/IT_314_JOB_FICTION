c70050339f5ba86720c65c4acd8a5c87d815290d
Karan Jivanramjiwala <karanhj786@gmail.com>
2023-12-04 13:46:29 +0530

    Files Deleted

--- Frontend/Company_profile/script.js
+++
@@ -1,9 +0,0 @@
-function viewmenu(){
-    // document.body.getElementsByClassName('main').classList.toggle('hide');
-    document.getElementById('hideshow').classList.toggle('hide');
-    let val = hideandshow();
-}
-
-function hideandshow(){
-    document.getElementById('showmenu').classList.toggle('active');
-}
--- Frontend/Main/Candidate_profile/script.js
+++
@@ -1,8 +0,0 @@
-function viewmenu(){
-    document.getElementById('hideshow').classList.toggle('hide');
-    let val = hideandshow();
-}
-
-function hideandshow(){
-    document.getElementById('showmenu').classList.toggle('active');
-}
